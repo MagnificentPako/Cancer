7ba9da90fa0a6326ae3618ef73091d20f2decc52
Paul <paul@niobium.me>
2017-08-04 23:11:19 +0200

    Some http stuff

--- proxy/index.js
+++ proxy/index.js
@@ -1,15 +1,35 @@
 const WebSocket = require("ws")
 const net = require("net")
+const tls = require("tls")
 
-const HOST = "127.0.0.1"
-const PORT = 6969
+const WSS_HOST = "127.0.0.1"
+const WSS_PORT = 6969
+
+const HTTP_HOST = "127.0.0.1"
+const HTTP_PORT = 4242
 
 var messages = []
 var socket = null
 
-var server = net.createServer();
-server.listen(PORT, HOST)
-server.on("connection", (sock) => {
+var http_server = net.createServer()
+http_server.listen(HTTP_PORT, HTTP_HOST)
+http_server.on("connection", (sock) => {
+    var s = tls.connect({host: "discordapp.com", port: 443}, () => {
+        sock.on("data", (data) => {
+            var d = data.toString()
+            s.write(d.replace("Host: localhost:4242", "Host: discordapp.com:443"))
+            console.log(d.replace("Authorization: Bot MzAxNjc5NzI3NTQwNjk5MTM2.DGZsiA.9ZGj7p-FLOm33_PmNQdSQQCxqeo\r\n", ""))
+        })
+        s.on("data", (data) => {
+            sock.write(data)
+            console.log(data.toString())
+        })
+    })
+})
+
+var wss_server = net.createServer()
+wss_server.listen(WSS_PORT, WSS_HOST)
+wss_server.on("connection", (sock) => {
 
     var wss_relay = []
 
@@ -31,12 +51,12 @@ server.on("connection", (sock) => {
         }else{
             if(wss_relay.length > 0) {
                 wss_relay.forEach((rel) => {
-                    console.log("relay<< " + JSON.stringify(JSON.parse(data)))
+                    //console.log("relay<< " + JSON.stringify(JSON.parse(data)))
                     wss.send(JSON.stringify(JSON.parse(data)))
                 })
                 wss_relay = []
             }
-            console.log("<< " + JSON.stringify(JSON.parse(data)))
+            //console.log("<< " + JSON.stringify(JSON.parse(data)))
             wss.send(JSON.stringify(JSON.parse(data)))
         }
     })
